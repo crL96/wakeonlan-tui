f07122c34ab9501a7973d5336b41a147dcfb8b05
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-16 16:22:40 +0100

    Init .net project

---
+++ wakeonlan-tui/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace wakeonlan_tui
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
