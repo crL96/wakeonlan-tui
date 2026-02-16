2c29264641cc688d2ff704a9d6f1fe3f587862da
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-16 17:48:17 +0100

    Add filehandler

---
+++ wakeonlan-tui/Data/FileHandler.cs
@@ -0,0 +1,42 @@
+using System.Text.Json;
+using wakeonlan_tui.Data.Models;
+
+namespace wakeonlan_tui.Data
+{
+    public static class FileHandler
+    {
+        private static string _filePath = @".\devices.json";
+
+        public static async Task AddNewDevice(Device device)
+        {
+            List<Device> devices = await GetAllDevices();
+            devices.Add(device);
+            string json = JsonSerializer.Serialize(devices);
+            await File.WriteAllTextAsync(_filePath, json);
+        }
+
+        public static async Task<List<Device>> GetAllDevices()
+        {
+            //If file doesn't exist, return empty list
+            try
+            {
+                string json = await File.ReadAllTextAsync(_filePath);
+                var list = JsonSerializer.Deserialize<List<Device>>(json);
+                if (list == null) return [];
+                return list;
+            }
+            catch (FileNotFoundException)
+            {
+                return [];
+            }
+        }
+
+        public static async Task DeleteDevice(string name)
+        {
+            List<Device> devices = await GetAllDevices();
+            var updatedList = devices.Where(d => d.Name != name);
+            string json = JsonSerializer.Serialize(updatedList);
+            await File.WriteAllTextAsync(_filePath, json);
+        }
+    }
+}
